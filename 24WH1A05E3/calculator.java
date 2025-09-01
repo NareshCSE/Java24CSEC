b420ceea019d83814dd9296d2146c5cffca9c11d
24wh1a05e3 <24wh1a05e3@bvrithyderabad.edu.in>
2025-09-01 22:07:56 +0530

    Add files via upload

---
+++ 24WH1A05E3/calculator.java
@@ -0,0 +1,32 @@
+package assignment;
+import java.util.Scanner;
+
+public class calculator {
+
+	public static void main(String[] args) {
+		Scanner sc= new Scanner(System.in);
+		System.out.println("enter first number:");
+		double a=sc.nextDouble();
+		System.out.println("enter second number:");
+		double b=sc.nextDouble();
+		System.out.println("enter operator(+,-,*,/):");
+		char op=sc.next().charAt(0);
+		
+		switch(op) {
+		case '+': System.out.println("result:"+(a+b));
+		break;
+		case '-': System.out.println("result:"+(a-b));
+        break;
+		case '*': System.out.println("result:"+(a*b));
+		break;
+		case '/': System.out.println("result:"+(a/b));
+		break;
+		default:System.out.println("invalid");
+		
+	}
+         sc.close();
+		
+
+	}
+
+}
