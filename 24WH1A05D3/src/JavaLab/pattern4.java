134edd4e50368faa843898ac807d24ad158753bd
24WH1A05D3 <islavathjyothi285@gmail.com>
2025-09-01 11:38:38 +0530

    Add files via upload

---
+++ 24WH1A05D3/src/JavaLab/pattern4.java
@@ -0,0 +1,19 @@
+package JavaLab;
+
+public class pattern4 {
+	public static void main(String[] args) {
+		
+    int n=5;
+		for(int i=1;i<=n;i++) { // i=rows
+			for(int j=1;j<=n-i;j++) { // j=spaces	
+				System.out.print(" ");
+	}
+			for(int k=1;k<=2*i-1;k++) { // k=*
+
+			System.out.print("*");
+		}
+		System.out.println();
+
+}
+}
+}
