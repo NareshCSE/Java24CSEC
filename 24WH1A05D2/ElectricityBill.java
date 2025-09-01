9bfb6f63c47c9f7f9bfb1b76e7267b533c20a189
Pravallika_D2 <24wh1a05d2@bvrithyderabad.edu.in>
2025-09-01 12:10:11 +0530

    Update ElectricityBill.java

--- 24WH1A05D2/ElectricityBill.java
+++ 24WH1A05D2/ElectricityBill.java
@@ -16,33 +16,34 @@ public class ElectricityBill {
         int currentReading = sc.nextInt();
 
         // Calculate units consumed
-        int unitsConsumed = currentReading - oldReading;
+        int units = currentReading - oldReading;
 
-        if (unitsConsumed < 0) {
+        if (units < 0) {
             System.out.println("Invalid readings.");
             return;
         }
 
-        double billAmount = 0;
+        double bill = 0;
 
         // Calculate the bill 
-        if (unitsConsumed <= 50) {
-            billAmount = unitsConsumed * 1;
-        } else if (unitsConsumed <= 100) {
-            billAmount = (50 * 1) + ((unitsConsumed - 50) * 2);
-        } else if (unitsConsumed <= 200) {
-            billAmount = (50 * 1) + (50 * 2) + ((unitsConsumed - 100) * 3);
-        } else if (unitsConsumed <= 400) {
-            billAmount = (50 * 1) + (50 * 2) + (100 * 3) + ((unitsConsumed - 200) * 4);
+        if (units <= 50) {
+            bill = units * 1;
+        } else if (units <= 100) {
+            bill = (50 * 1) + ((units - 50) * 2);
+        } else if (units <= 200) {
+            bill = (50 * 1) + (50 * 2) + ((units - 100) * 3);
+        } else if (units <= 400) {
+            bill = (50 * 1) + (50 * 2) + (100 * 3) + ((units - 200) * 4);
         } else {
-            billAmount = (50 * 1) + (50 * 2) + (100 * 3) + (200 * 4) + ((unitsConsumed - 400) * 5);
+            bill = (50 * 1) + (50 * 2) + (100 * 3) + (200 * 4) + ((units - 400) * 5);
         }
 
         // Display the result
-        System.out.println("Units Consumed: " + unitsConsumed);
-        System.out.println("Total Bill: Rs." + billAmount);
+        System.out.println("Units Consumed: " + units);
+        System.out.println("Total Bill: Rs." + bill);
 
         sc.close();
     }
 }
+
 
