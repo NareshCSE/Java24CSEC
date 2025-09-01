1c44b44063bcfd752c2a8826466b39eb54c92e5b
24wh1a05g4 <24wh1a05g4@bvrithyderabad.edu.in>
2025-09-01 19:35:38 +0530

    Add files via upload

--- calculator.java
+++ calculator.java
@@ -1,36 +1,31 @@
-package cycle1;
-import java.util.*;
+package calci;
+import java.util.Scanner;
 public class calculator {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-        Scanner read = new Scanner(System.in);
-        int num1,num2,res;
-        char op;
-        num1 = read.nextInt();
-        num2=read.nextInt();
-        op= read.next().charAt(0);
-        switch(op) {
-        case '+' :
-        	      res = num1+num2;
-        	      System.out.println("sum is " + res);
-        	      break;
-        case '-':
-        	      res = num1-num2;
-        	      System.out.println("sub is "+ res);
-        	      break;
-        case '*':
-        	      res = num1*num2;
-        	      System.out.println("mul is "+ res);
-        	      break;
-        case '/':
-        	      res = num1/num2;
-        	      System.out.println("div is " + res);
-        	      break;
-        default :
-        	      System.out.println("invalid");
-        
-        }
+		Scanner sc= new Scanner(System.in);
+		System.out.println("enter first number:");
+		double a=sc.nextDouble();
+		System.out.println("enter second number:");
+		double b=sc.nextDouble();
+		System.out.println("enter operator(+,-,*,/):");
+		char op=sc.next().charAt(0);
+		
+		switch(op) {
+		case '+': System.out.println("result:"+(a+b));
+		break;
+		case '-': System.out.println("result:"+(a-b));
+        break;
+		case '*': System.out.println("result:"+(a*b));
+		break;
+		case '/': System.out.println("result:"+(a/b));
+		break;
+		default:System.out.println("invalid");
+		
+	}
+         sc.close();
+	
+	
 	}
 
 }
