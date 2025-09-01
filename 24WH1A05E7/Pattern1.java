34240ded2340910bfe3ad1dc78369080e2731aa4
LAXMIPRIYA REDDY <24wh1a05e7@bvrithyderabad.edu.in>
2025-09-01 12:15:02 +0530

    Add files via upload

---
+++ 24WH1A05E7/Pattern1.java
@@ -0,0 +1,23 @@
+package priya;
+
+import java.util.Scanner;
+
+public class Pattern1 {
+
+	public static void main(String[] args) {
+		int rows=5,i,j;
+		for(i=1; i<=rows; i++)
+		{
+			for(j=1; j<=i; j++)
+			{
+				System.out.print("* ");
+		
+			}
+			    System.out.println();
+		}
+
+	}
+
+}
+
+
