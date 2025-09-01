5e6ecf4bc541bb3b66a6db4e2a4c4b64d46ee657
Narahari Vaishnavi <24wh1a05d8@bvrithyderabad.edu.in>
2025-09-01 18:41:57 +0530

    Add files via upload

---
+++ 24WH1A05D8/exp_5c.java
@@ -0,0 +1,26 @@
+package basics;
+import java.util.Scanner;
+public class exp_5c {
+
+	public static void main(String[] args) {
+		String[] str1=new String[3];
+		String[] str2=new String[3];
+		Scanner s=new Scanner(System.in);
+		System.out.println("Enter string1 elements:");
+		for(int i=0;i<str1.length;i++) {
+			str1[i]=s.next();
+		}
+		System.out.println("Enter string2 elements:");
+		for(int i=0;i<str2.length;i++) {
+			str2[i]=s.next();
+	}
+		for(int i=0;i<str1.length;i++) {
+			for(int j=0;j<str2.length;j++) {
+				if(str1[i].equals(str2[j]))
+					System.out.print(str2[j]+" ");
+			}
+		}
+
+	}
+
+}
---
+++ 24WH1A05D8/pattern4.java
@@ -0,0 +1,22 @@
+package basics;
+
+public class pattern4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i,j;
+		for(i=1;i<=5;i++) {
+	        for (j=1;j<=5-i;j++) {
+	            System.out.print(" ");
+	        }
+	        for(j=1;j<=i;j++) {
+	            System.out.print(j);
+	        }
+	        for(j=i-1;j>=1;j--) {
+	            System.out.print(j);
+	        }
+	        System.out.println();
+	    }
+	}
+
+}
