9636e76ca1434d7302836753c377d5844d59d37a
24WH1A05D3 <islavathjyothi285@gmail.com>
2025-09-02 19:35:57 +0530

    Add files via upload

---
+++ 24WH1A05D3/pattern4.java
@@ -0,0 +1,23 @@
+package javaAssignment;
+
+public class pattern4 {
+
+	public static void main(String[] args) {
+		int n=5;
+		for(int i=1;i<=5;i++) {
+			for(int j=1;j<=n-i;j++) {
+				System.out.print(" ");
+			}
+		    for (int k = 1; k <= i; k++) {
+                System.out.print(k);
+			}
+			  for (int k = i - 1; k >= 1; k--) {
+	                System.out.print(k);
+	            }
+			System.out.print("\n");
+		}
+	
+
+	}
+
+}
