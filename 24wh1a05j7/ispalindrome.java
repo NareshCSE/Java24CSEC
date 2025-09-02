53b50193251467fb4b22cce78b00d9ae44421daa
24WH1A05J7 <emandibhavani1410@gmail.com>
2025-09-02 15:51:25 +0530

    Added palindrome java program

---
+++ 24wh1a05j7/ispalindrome.java
@@ -0,0 +1,51 @@
+public class ispalindrome {
+
+
+
+	public static void main(String[] args) {
+
+		// TODO Auto-generated method stub
+
+		int number=120,remainder,target,sum=0;
+
+		while(number!=0) {
+
+		 target=number;
+
+		 sum=0;
+
+		 while(target>0)
+
+		 {
+
+			 remainder=target%10;
+
+			 target=target/10;
+
+			 sum=sum*10+remainder;
+
+		 }
+
+		 if(number==sum)
+
+		 {
+
+			 System.out.println( number+"palindrome");
+
+			 break;
+
+		 }
+
+		 else {
+
+			 number=number+1;
+
+		 }
+
+	
+
+	}
+
+
+
+}}
