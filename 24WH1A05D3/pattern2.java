b9bc9a61dcd0232750705d427e3be9e4b60e0ed5
24WH1A05D3 <islavathjyothi285@gmail.com>
2025-09-02 19:26:55 +0530

    Add files via upload

---
+++ 24WH1A05D3/pattern2.java
@@ -0,0 +1,28 @@
+package javaAssignment;
+
+public class pattern2 {
+ 
+
+	public static void main(String[] args) {
+		//print the following pattern
+				//         *
+				//       * *
+				//     * * *
+				//   * * * *
+				// * * * * *
+		int i, j, k, rows=5;
+		for(i=1; i<=rows; i++)
+		{
+			for(j=1; j<=rows-i; j++)
+			{
+				System.out.print(" ");
+			}
+			for(k=1; k<=i; k++)
+			{
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+
+}
+}
