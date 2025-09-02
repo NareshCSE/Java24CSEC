6e96462b0a9efb78c5e605d9c3a96ee75803c941
Sriharshitha <sriharshithapadigelwar@gmail.com>
2025-09-02 15:36:50 +0530

    Add files via upload

---
+++ 24WH1A05F3/pattern1.java
@@ -0,0 +1,16 @@
+
+public class pattern1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i;int j;
+		for(i=0;i<5;i++) {
+			for(j=0;j<=i;j++) {
+				System.out.print("* ");
+			}
+			System.out.println();
+		}
+
+	}
+
+}
