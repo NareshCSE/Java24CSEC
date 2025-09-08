add26939ec2413951d69ecabe00fc6218c68bbc6
novicecode050 <tanoojaml@gmail.com>
2025-09-08 11:19:26 +0530

    Add files via upload

---
+++ 24WH1A05E1/SumEvenOdd.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class SumEvenOdd {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        
+        System.out.print("Enter how many numbers you want to input: ");
+        int n = sc.nextInt();
+
+        int evenSum = 0, oddSum = 0;
+
+        
+        System.out.println("Enter " + n + " integers:");
+        for (int i = 0; i < n; i++) {
+            int num = sc.nextInt();
+
+            if (num % 2 == 0) {
+                evenSum += num; 
+            } else {
+                oddSum += num; 
+            }
+        }
+
+       
+        System.out.println("Sum of even numbers = " + evenSum);
+        System.out.println("Sum of odd numbers = " + oddSum);
+
+        sc.close();
+    }
+}
