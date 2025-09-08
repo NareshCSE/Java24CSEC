8a846bdbe5c82c871c2b8832a42d51d75877c5c6
25wh5a0519 <25wh5a0519@bvrithyderabad.edu.in>
2025-09-08 09:42:28 +0530

    Add files via upload

---
+++ 25WH5A0521/For1.java
@@ -0,0 +1,30 @@
+package file.javac;
+import java.util.Scanner;
+
+
+
+public class For1 {
+
+  public static void main(String[] args) {
+
+		Scanner sc=new Scanner(System.in);
+
+		System.out.println("Enter the number: ");
+
+		int size=sc.nextInt();
+
+		for(int i=1;i<=size;i++) {
+
+			for(int j=1;j<=i;j++){
+
+				System.out.print("* ");
+
+			}
+
+			System.out.println();// Move to the next row
+
+			}
+
+		}
+
+}
