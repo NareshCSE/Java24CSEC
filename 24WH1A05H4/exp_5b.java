b27d13985f8fdc73ee641659a04e1cd65bc33cc5
Preethi <agspreethiayinampudi@gmail.com>
2025-09-08 09:27:48 +0530

    exp_5b.java

--- 24WH1A05H4/exp_5b.java
+++ 24WH1A05H4/exp_5b.java
@@ -1,5 +1,41 @@
 package javacodes;
 
+import java.util.Scanner;
 public class exp_5b {
 
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[][] arr1=new int[2][2];
+		int[][] arr2=new int[2][2];
+		Scanner input=new Scanner(System.in);
+		System.out.println("enter arr1 elements:");
+		for(int i=0;i<2;i++) {
+			for(int j=0;j<2;j++)
+			arr1[i][j]=input.nextInt();
+		}
+		System.out.println("enter arr2 elements:");
+		for(int i=0;i<2;i++) {
+			for(int j=0;j<2;j++)
+			arr2[i][j]=input.nextInt();
+		}
+		int[][] result=new int[2][2];
+		for(int i=0;i<2;i++) {
+			for(int j=0;j<2;j++) {
+				result[i][j]=0;
+				for(int k=0;k<2;k++) {
+					result[i][j]+=arr1[i][k]*arr2[k][j];
+				}
+			}
+		}
+		for(int i=0;i<2;i++) {
+			for(int j=0;j<2;j++) {
+				System.out.print(result[i][j]+" ");
+			}
+			System.out.println();
+		}
+	}
+
 }
+
+}
+
