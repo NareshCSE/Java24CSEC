be695138d5453d703d9253abb99c9aee256ed7b4
LAXMIPRIYA REDDY <24wh1a05e7@bvrithyderabad.edu.in>
2025-09-08 11:00:44 +0530

    Add files via upload

---
+++ 24WH1A05E7/LargestEvenNumber.java
@@ -0,0 +1,71 @@
+
+public class LargestEvenNumber {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 String str = "ad%#678#@";   
+
+	        boolean[] present = new boolean[10]; 
+
+	        for (int i = 0; i < str.length(); i++) {
+
+	            char ch = str.charAt(i);
+
+	            if (Character.isDigit(ch)) {
+
+	                present[ch - '0'] = true;
+
+	            }
+
+	        }
+
+	        int smallestEven = -1;
+
+	        for (int d = 0; d <= 9; d++) {
+
+	            if (present[d] && d % 2 == 0) {
+
+	                smallestEven = d;
+
+	                break;
+
+	            }
+
+	        }
+
+
+
+	        
+
+	        if (smallestEven == -1) {
+
+	            System.out.println(-1);
+
+	            return;
+
+	        }
+
+
+
+	        System.out.println("largest even no:");
+
+	        
+
+	        for (int d = 9; d >= 0; d--) {
+
+	            if (present[d] && d != smallestEven) {
+
+	                System.out.print(d);
+
+	            }
+
+	        }
+
+
+
+	     System.out.println(smallestEven);
+
+	       
+	}
+
+}
