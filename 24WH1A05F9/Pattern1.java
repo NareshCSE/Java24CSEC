b9b7677ee81cc0d7e826d346c78e6232c39eee79
VinuthnaSabbani <24wh1a05f9@bvrithyderabad.edu.in>
2025-09-08 11:37:19 +0530

    Add files via upload

---
+++ 24WH1A05F9/Pattern1.java
@@ -0,0 +1,21 @@
+package labprograms;
+import java.util.Scanner;
+
+public class Pattern1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter n value");
+		int n=sc.nextInt();
+		int i,j;
+		for(i=1;i<=n;i++) {
+			for(j=1;j<=i;j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+
+	}
+
+}
