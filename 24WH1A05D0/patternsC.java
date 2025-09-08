30985e5d46b753a50f2828c3c67e882561d14d69
thanmai-19 <24wh1a05d0@bvrithyderabad.edu.in>
2025-09-08 10:38:23 +0530

    Add files via upload

---
+++ 24WH1A05D0/patternsC.java
@@ -0,0 +1,33 @@
+package javaprograms;
+
+public class patternsC {
+    public static void main(String[] args) {
+        int n = 5; // number of rows for half
+
+        // Top half of diamond
+        for (int i = 1; i <= n; i++) {
+            // Print spaces
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+            // Print stars
+            for (int k = 1; k <= i; k++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+
+        // Bottom half of diamond
+        for (int i = n - 1; i >= 1; i--) {
+            // Print spaces
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+            // Print stars
+            for (int k = 1; k <= i; k++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
