29d9c01c206f6f6ed00eaded1068fd6779297d5f
24wh1a05e3 <24wh1a05e3@bvrithyderabad.edu.in>
2025-09-08 15:25:55 +0530

    Add files via upload

---
+++ 24WH1A05G4/even_oddsum.java
@@ -0,0 +1,29 @@
+package assignment;
+import java.util.Scanner;
+
+public class even_oddsum {
+
+	public static void main(String[] args) {
+		 Scanner scanner = new Scanner(System.in);
+	        System.out.print("Enter the number of integers you want to enter: ");
+	        int count = scanner.nextInt();
+	        int sumEven = 0;
+	        int sumOdd = 0;
+	        System.out.println("Enter " + count + " integers:");
+	        for (int i = 0; i < count; i++) {
+	            System.out.print("Enter integer " + (i + 1) + ": ");
+	            int num = scanner.nextInt();
+	            if (num % 2 == 0) {
+	                sumEven += num;
+	            } 
+	            else {
+	                sumOdd += num;
+	            }
+	        }
+	        System.out.println("Sum of even integers: " + sumEven);
+	        System.out.println("Sum of odd integers: " + sumOdd);
+	        scanner.close();
+
+	}
+
+}
