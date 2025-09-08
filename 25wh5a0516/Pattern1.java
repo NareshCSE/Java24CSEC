a99d13e9ddb650e983a541843c26abeb649bd6ab
snigdhagatla <25wh5a0516@bvrithyderabad.edu.in>
2025-09-08 11:36:24 +0530

    Add files via upload

---
+++ 25wh5a0516/Pattern1.java
@@ -0,0 +1,17 @@
+package javademo;
+
+public class Pattern1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int i,j;
+int rows=5;
+for(i=0;i<=rows;i++) {
+	for(j=0;j<=i;j++) {
+	System.out.print("*");
+}
+	System.out.println();
+	}
+
+}
+}
