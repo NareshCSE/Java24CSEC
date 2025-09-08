49471e6d0c0463ba8d4b2c17f9181cee22e023d8
Mahimakrishnamurthy <24wh1a05f2@bvrithyderabad.edu.in>
2025-09-08 11:02:46 +0530

    Add files via upload

---
+++ 24WH1A05F2/mulOf2Mat_5b.java
@@ -0,0 +1,44 @@
+package java_lab_exp;
+import java.util.Scanner;
+public class mulOf2Mat_5b {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[][] arr1=new int[2][2];
+		int[][] arr2=new int[2][2];
+		Scanner sc = new Scanner(System.in);
+        System.out.println("Enter arr1 elements: ");
+        for(int i=0; i<2; i++) {
+        	for(int j=0; j<2; j++) {
+        		arr1[i][j] = sc.nextInt();
+        	}
+	}
+        System.out.println("Enter arr2 elements: ");
+        for(int i=0; i<2; i++) {
+        	for(int j=0; j<2; j++) {
+        		arr2[i][j] = sc.nextInt();
+        	}
+        }
+        int[][] result = new int[2][2];
+        for(int i=0; i<2; i++) {
+        	for(int j=0; j<2; j++) {
+        		result[i][j] = 0;
+        		for(int k=0; k<2; k++) {
+        			result[i][j]+=arr1[i][k]*arr2[k][j];
+        		}
+        	}
+        }
+        for(int i=0; i<2; i++) {
+        	for(int j=0; j<2; j++) {
+        		System.out.print(result[i][j]+" ");
+        	}
+        }
+        System.out.println();
+	}
+}
+
+        
+        
+        
+    
+	
