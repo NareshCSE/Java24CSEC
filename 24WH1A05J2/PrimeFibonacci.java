1f83878b1baae289b0f828ee769c0b998476b515
K_SRI VAISHNAVI <Kallepusrivaishnavi@gmail.com>
2025-09-08 11:43:32 +0530

    Add files via upload

---
+++ 24WH1A05J2/PrimeFibonacci.java
@@ -0,0 +1,45 @@
+package javac;
+
+public class PrimeFibonacci {
+
+	public static boolean isPrime(int num) {
+		if(num<=1) {
+			return false;
+		}
+		else {
+			for(int i=2;i<=Math.sqrt(num);i++) {
+				if(num%i==0) {
+					return false;
+				}
+			}
+			return true;
+		}
+
+	}
+	public static boolean isFibonacci(int n) {
+		int a=0,b=1;
+		while(a<=n) {
+			if(a==n)
+			return true;
+			int c=a+b;
+			a=b;
+			b=c;
+			
+		}
+		return false;
+	}
+		
+	public static void main(String[] args) {
+		int n = 10;  
+        System.out.print("Prime Fibonacci numbers between 1 and " + n + " are: ");
+        for (int i = 2; i <= n; i++) {
+            if (isPrime(i) && isFibonacci(i)) {
+                System.out.print(i + " ");
+            }
+        }	
+	}
+		
+	}
+
+
+   
