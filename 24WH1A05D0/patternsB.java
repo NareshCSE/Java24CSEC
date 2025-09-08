b8d847153c9cfe8012a451b98ae03fd75e62afc5
thanmai-19 <24wh1a05d0@bvrithyderabad.edu.in>
2025-09-08 10:36:21 +0530

    Add files via upload

---
+++ 24WH1A05D0/patternsB.java
@@ -0,0 +1,19 @@
+package javaprograms;
+
+public class patternsB {
+    public static void main(String[] args) {
+        int n = 5; // number of rows
+
+        for (int i = 1; i <= n; i++) {
+            // Print spaces
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print("  ");
+            }
+            // Print stars
+            for (int k = 1; k <= i; k++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
