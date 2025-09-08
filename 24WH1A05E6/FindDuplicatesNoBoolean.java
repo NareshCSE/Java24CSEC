d9f74c160761db176aeff0920558882562013657
kaumudicodes <24wh1a05e6@bvrithyderabad.edu.in>
2025-09-08 10:55:19 +0530

    Add files via upload

---
+++ 24WH1A05E6/FindDuplicatesNoBoolean.java
@@ -0,0 +1,36 @@
+package Assignment;
+public class FindDuplicatesNoBoolean {
+    public static void main(String[] args) {
+        int[] numbers = {1, 2, 3, 2, 4, 5, 5, 6, 7, 3};
+        findDuplicates(numbers);
+    }
+
+    public static void findDuplicates(int[] arr) {
+        System.out.print("Duplicate values: ");
+
+        for (int i = 0; i < arr.length; i++) {
+            // Check if already printed
+            int alreadyPrintedCount = 0;
+            for (int k = 0; k < i; k++) {
+                if (arr[i] == arr[k]) {
+                    alreadyPrintedCount++;
+                }
+            }
+            if (alreadyPrintedCount > 0) {
+                continue;
+            }
+            int duplicateCount = 0;
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[i] == arr[j]) {
+                    duplicateCount++;
+                }
+            }
+
+            if (duplicateCount > 0) {
+                System.out.print(arr[i] + " ");
+            }
+        }
+
+        System.out.println();
+    }
+}
