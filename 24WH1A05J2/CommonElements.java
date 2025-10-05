2f756bcbc03eef26acc4c7a5d23be39d35d11eb0
K_SRI VAISHNAVI <Kallepusrivaishnavi@gmail.com>
2025-10-05 22:26:54 +0530

    Add files via upload

---
+++ 24WH1A05J2/CommonElements.java
@@ -0,0 +1,17 @@
+package srivaishnavi;
+public class CommonElements {
+    public static void main(String[] args) {
+        String arr1[] = {"apple", "banana", "cherry", "mango"};
+        String arr2[] = {"grapes", "banana", "orange", "mango"};
+
+        System.out.println("Common elements:");
+        for(int i=0; i<arr1.length; i++) {
+            for(int j=0; j<arr2.length; j++) {
+                if(arr1[i].equals(arr2[j])) {
+                    System.out.println(arr1[i]);
+                }
+            }
+        }
+    }
+}
+
