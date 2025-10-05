00f7f43a9bb785385d78783abd817a3315104d88
K_SRI VAISHNAVI <Kallepusrivaishnavi@gmail.com>
2025-10-05 19:51:02 +0530

    Add files via upload

---
+++ 24WH1A05J2/bankinheritance.java
@@ -0,0 +1,38 @@
+package srivaishnavi;
+class Banks {
+    int getBalance() {
+        return 0;
+    }
+}
+
+class BankA extends Banks {
+    int getBalance() {
+        return 1000;
+    }
+}
+
+class BankB extends Banks {
+    int getBalance() {
+        return 1500;
+    }
+}
+
+class BankC extends Banks {
+    int getBalance() {
+        return 2000;
+    }
+}
+
+public class bankinheritance {
+    public static void main(String[] args) {
+        BankA a = new BankA();
+        BankB b = new BankB();
+        BankC c = new BankC();
+        System.out.println("Money in Bank A: $" + a.getBalance());
+        System.out.println("Money in Bank B: $" + b.getBalance());
+        System.out.println("Money in Bank C: $" + c.getBalance());
+    }
+}
+
+
+
