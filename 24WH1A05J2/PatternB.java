6d8b4cdef06add793d65c2ce0a9b5ab9f0a1eafe
K_SRI VAISHNAVI <Kallepusrivaishnavi@gmail.com>
2025-10-05 19:31:39 +0530

    Add files via upload

---
+++ 24WH1A05J2/PatternB.java
@@ -0,0 +1,18 @@
+package srivaishnavi;
+
+public class PatternB {
+	public static void main(String[] args) {
+		
+        int n = 5; // number of rows
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print("  "); // spaces
+            }
+            for (int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+
+}
