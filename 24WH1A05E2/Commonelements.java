36f76ba2611f692bd384c4375c5ea1792fd26438
Greeshmitha20 <24wh1a05e2@bvrithyderabad.edu.in>
2025-10-06 10:57:35 +0530

    Add files via upload

---
+++ 24WH1A05E2/Commonelements.java
@@ -0,0 +1,16 @@
+
+public class Commonelements{
+    public static void main(String[] args) {
+        String arr1[] = {"chocolate", "banana", "cherry", "mango"};
+        String arr2[] = {"chocolate", "banana", "orange", "mango"};
+
+        System.out.println("Common elements:");
+        for(int i=0; i<arr1.length; i++) {
+            for(int j=0; j<arr2.length; j++) {
+                if(arr1[i].equals(arr2[j])) {
+                    System.out.println(arr1[i]);
+                }
+            }
+        }
+    }
+}
