7b547b4039ab54f956004dac4eade8753ce42503
Sriharshitha <sriharshithapadigelwar@gmail.com>
2025-10-06 09:22:35 +0530

    Add files via upload

---
+++ 24WH1A05F3/pattern2.java
@@ -0,0 +1,22 @@
+package com.studyopedia;
+
+public class pattern2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i ,j,k,n=5;
+		for(i=1;i<=n;i++) {
+			for(j=n-i;j>0;j--) {
+				System.out.print("  ");
+			}
+			for(k=1;k<=i;k++) {
+				System.out.print(" *");
+			}
+			System.out.println();
+			
+         }
+	}
+}
+
+
+
