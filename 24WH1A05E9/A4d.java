8d2028b1f8dd6e82be299c261749f58dc11c70ad
Krishna Lahari Gudimella <krishnalahari1411@gmail.com>
2025-10-06 10:51:51 +0530

    Add files via upload

---
+++ 24WH1A05E9/A4d.java
@@ -0,0 +1,28 @@
+package e9;
+
+public class A4d {
+
+    public static void main(String[] args) {
+        int rows = 5;
+
+        for (int i = 1; i <= rows; i++) {
+            // Print leading spaces
+            for (int space = 1; space <= rows - i; space++) {
+                System.out.print(" ");
+            }
+
+            // Print increasing numbers
+            for (int num = 1; num <= i; num++) {
+                System.out.print(num);
+            }
+
+            // Print decreasing numbers
+            for (int num = i - 1; num >= 1; num--) {
+                System.out.print(num);
+            }
+
+            // Move to next line
+            System.out.println();
+        }
+    }
+}
