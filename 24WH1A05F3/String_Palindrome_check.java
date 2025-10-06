49b5e0abd25b9533dc03f212805239a884aa302f
Sriharshitha <sriharshithapadigelwar@gmail.com>
2025-10-06 10:39:08 +0530

    Add files via upload

---
+++ 24WH1A05F3/String_Palindrome_check.java
@@ -0,0 +1,24 @@
+package com.studyopedia;
+import java.util.Scanner;
+
+public class String_Palindrome_check {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the string:");
+        String str = sc.next();
+        String temp = str;
+        System.out.println("Original String: " + temp);
+        
+        
+        String reversed = new StringBuilder(str).reverse().toString();
+        System.out.println("Reversed String: " + reversed);
+        
+        if(temp.equals(reversed)) {
+            System.out.println("The string is a palindrome.");
+        } else {
+            System.out.println("The string is not a palindrome.");
+        }
+
+        sc.close();
+    }
+}
