f25863477a07b216cd8200100b65663c7871ced2
RHanshika <24wh1a05j1@bvrithyderabad.edu.in>
2025-10-06 10:01:05 +0530

    Update Multiplication_ofMatrices.java

--- 24WH1A05J1/java5J1/Multiplication_ofMatrices.java
+++ 24WH1A05J1/java5J1/Multiplication_ofMatrices.java
@@ -1,23 +1,42 @@
-package java5J1;
+package src;
 import java.util.Scanner;
-public class Multiplication_ofMatrices {
+public class MultiplicationOfTwoMatrices {
 
 	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[][] array1 = new int[2][2];
+		int[][] array2 = new int[2][2];
+		int[][] resultArray = new int[2][2];
 		Scanner sc = new Scanner (System.in);
-		System.out.println("Enter no.of rows and columns in matA:");
-		int rows1 = sc.nextInt();
-		int cols1 = sc.nextInt();
-		System.out.println("Enter no.of rows and columns in matB:");
-		int rows2 = sc.nextInt();
-		int cols2 = sc.nextInt();
-		int[][] matA = new int[rows1][cols1];
-		int[][] matB = new int[rows2][cols2];
+		System.out.println("Enter the 4 elements of array1: ");
+		for (int i = 0; i < 2; i++) {
+			for (int j = 0; j < 2; j++) {
+				array1[i][j] = sc.nextInt();
+			}
+		}
+		System.out.println("Enter the 4 elements of array2: ");
+		for (int i = 0; i < 2; i++) {
+			for (int j = 0; j < 2; j++) {
+				array2[i][j] = sc.nextInt();
+			}
+		}
+		for (int i = 0; i < 2; i++) {
+			for (int j = 0; j < 2; j++) {
+				resultArray[i][j] = 0;
+				for (int k = 0; k < 2; k ++) {
+					resultArray[i][j] += array1[i][k] * array2[k][j];
+				}
+			}
+		}
+		System.out.println("The resultant multiplication matrix is: ");
+		for (int i = 0; i < 2; i++) {
+			for (int j = 0; j < 2; j++) {
+				System.out.print(resultArray[i][j] + " ");
+			}
+			System.out.println();
+		}
 		
-		
-		
-		
-		
-		
+		sc.close();
 
 	}
 
