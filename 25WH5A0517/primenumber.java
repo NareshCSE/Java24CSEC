0bdfc8a4d4456dad69a7b423f0c26f19519b1db8
navyasree41 <hambadas016@gmail.com>
2025-10-06 10:34:54 +0530

    Add files via upload

---
+++ 25WH5A0517/primenumber.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class primenumber {
+    
+    
+    public static boolean isPrime(int num) {
+        if (num <= 1) {
+            return false;
+        }
+        for (int i = 2; i * i <= num; i++) {
+            if (num % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
