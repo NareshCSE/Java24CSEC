04b3e619d5cc7b2f927658008326f77428322296
Krishna Lahari Gudimella <krishnalahari1411@gmail.com>
2025-10-10 05:39:08 +0530

    Add files via upload

---
+++ 24WH1A05E9/A10.java
@@ -0,0 +1,57 @@
+package practicefiles;
+class Bank {
+    protected int balance;
+
+    Bank(int balance) {
+        this.balance = balance;
+    }
+
+    int getBalance() {
+        return 0; // base class default implementation
+    }
+}
+
+class BankA extends Bank {
+    BankA(int balance) {
+        super(balance);
+    }
+
+    int getBalance() {
+        return balance;
+    }
+}
+
+class BankB extends Bank {
+    BankB(int balance) {
+        super(balance);
+    }
+
+    int getBalance() {
+        return balance;
+    }
+}
+
+class BankC extends Bank {
+    BankC(int balance) {
+        super(balance);
+    }
+
+    int getBalance() {
+        return balance;
+    }
+}
+
+public class A10 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		BankA a = new BankA(1000);
+        BankB b = new BankB(1500);
+        BankC c = new BankC(2000);
+
+        System.out.println("Amount deposited in Bank A: $" + a.getBalance());
+        System.out.println("Amount deposited in Bank B: $" + b.getBalance());
+        System.out.println("Amount deposited in Bank C: $" + c.getBalance());
+	}
+
+}
