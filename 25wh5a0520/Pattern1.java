6c4e0131843741a29fffe39dd848d3be025e84fd
gangushirisha02 <gangushirisha2007@gmail.com>
2025-10-13 09:31:54 +0530

    Add files via upload

---
+++ 25wh5a0520/Pattern1.java
@@ -0,0 +1,18 @@
+package javaprograms;
+
+public class Pattern1 {
+
+	public static void main(String[] args) {
+		int n=5;
+		// TODO Auto-generated method stub
+		for(int i=0;i<n;i++)
+		{
+			for(int j=0;j<=i;j++)
+			{
+				System.out.print("*");
+			}
+			System.out.println( );
+		}
+	}
+
+}
