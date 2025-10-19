781245e93ac3a86fb4efd166f45a82beed55d622
sangamithra-ui <24wh1a05e5@bvrithyderabad.edu.in>
2025-10-19 22:17:39 +0530

    Add files via upload

---
+++ 24WH1A05E5/Patternc.java
@@ -0,0 +1,29 @@
+package patternc;
+
+public class Patternc {
+	public static void main() {
+		  int n = 5; // half rows (upper part)
+
+	        // upper part
+	        for (int i = 1; i <= n; i++) {
+	            for (int j = i; j < n; j++) {
+	                System.out.print("  ");
+	            }
+	            for (int j = 1; j <= (2 * i - 1); j++) {
+	                System.out.print("* ");
+	            }
+	            System.out.println();
+	        }
+
+	        // lower part
+	        for (int i = n - 1; i >= 1; i--) {
+	            for (int j = n; j > i; j--) {
+	                System.out.print("  ");
+	            }
+	            for (int j = 1; j <= (2 * i - 1); j++) {
+	                System.out.print("* ");
+	            }
+	            System.out.println();
+	        }
+	}
+}
