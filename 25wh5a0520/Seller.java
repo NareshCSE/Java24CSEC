ca80e67640d5150962d749140d3f31e7b5572936
gangushirisha02 <gangushirisha2007@gmail.com>
2025-11-10 10:14:31 +0530

    Add files via upload

---
+++ 25wh5a0520/Seller.java
@@ -0,0 +1,29 @@
+package assignments;
+import java.util.*;
+interface Ecommers
+{
+	 static void addProduct(Product product) {
+	}
+	 static Set<Product> getProducts()
+	 {
+		 
+	 }
+	 order placeorder(set<product>)
+	 {
+		 
+	 }
+	 double generatebill(int order_id)
+	 {
+		 
+	 }
+}
+
+public class Seller {
+
+	public static void main(String[] args) {
+		
+		
+
+	}
+
+}
