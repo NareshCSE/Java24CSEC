513563a37f88e9b955dfd41db8674df9d54744d4
Chakali_Malleshwari-gif <24wh1a05d6@bvrithyderabad.edu.in>
2025-11-17 11:29:33 +0530

    Add files via upload

---
+++ 24WH1A05D6/SimpleAWTExample.java
@@ -0,0 +1,8 @@
+import java.awt.*;  
+public class SimpleAWTExample {
+    public static void main(String[] args) {
+        Frame frame = new Frame("AWT Example");
+        frame.setSize(300, 200);
+        frame.setVisible(true);
+    }
+}
