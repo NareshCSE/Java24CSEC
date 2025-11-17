0449e1522ed844802a149dede6273fc650816e73
gangushirisha02 <gangushirisha2007@gmail.com>
2025-11-17 20:56:56 +0530

    Add files via upload

--- 25wh5a0520/Calculator.java
+++ 25wh5a0520/Calculator.java
@@ -1,56 +1,44 @@
-package practice;
-import java.awt.*;
-import javax.swing.*;
-import java.awt.event.*;
-
-
+package java_25wh5a0520;
+import java.util.*;
 public class Calculator {
-   public static void main(String[] args) {
-	   JFrame frame=new JFrame();
-	     Calculator(){
-		   
-	   
-	   frame.setVisible(true);
-	   frame.setLayout(null);
-	   frame.setBounds( 100,50,500, 600);
-	   GridLayout g=new GridLayout(4,4);
-	   TextField t=new TextField();
-	   
-	   Button n1=new Button();
-	   Button n2=new Button();
-	   Button n3=new Button();
-	   Button n4=new Button();
-	   Button n5=new Button();
-	   Button n6=new Button();
-	   Button n7=new Button();
-	   Button n8=new Button();
-	   Button n9=new Button();
-	   Button mul=new Button();
-	   Button add=new Button();
-	   Button div=new Button();
-	   Button sub=new Button();
-	   Button equal=new Button();
-	   g.addLayoutComponent("textfeild", t);
-	   g.addLayoutComponent("1", n1);
-	   g.addLayoutComponent("2", n2);
-	   g.addLayoutComponent("3", n3);
-	   g.addLayoutComponent("+", add);
-	   g.addLayoutComponent("4", n4);
-	   g.addLayoutComponent("5", n5);
-	   g.addLayoutComponent("6", n6);
-	   g.addLayoutComponent("-", sub);
-	   g.addLayoutComponent("7", n7);
-	   g.addLayoutComponent("8", n8);
-	   g.addLayoutComponent("9", n9);
-	   g.addLayoutComponent("*", mul);
-	   g.addLayoutComponent("=", equal);
-	   g.addLayoutComponent("/", div);
-	   frame.add();
-	   frame.setVisible(true);
-	   frame.setLayout(null);
-	   frame.setBounds( 100,50,500, 600);
-	   }
-	  
+
+	public static void main(String[] args) {
+		int num1, num2, res;
+		char ch;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter number1: ");
+		num1 = sc.nextInt();
+		System.out.println("Enter number2: ");
+		num2 = sc.nextInt();
+		System.out.println("Enter operator: ");
+		ch = sc.next().charAt(0);
+		switch(ch) {
+			case '+':
+				res = num1 + num2;
+				System.out.println("result= " + res);
+				break;
+			case '-':
+				res = num1 - num2;
+				System.out.println("result= " + res);
+				break;
+			case '*':
+				res = num1 * num2;
+				System.out.println("result= " + res);
+				break;
+			case '/':
+				res = num1/num2;
+				System.out.println("result= " + res);
+				break;
+			case '%':
+                                                      res = num1 % num2;
+				System.out.println("result= " + res);
+				break;
+			default:
+				System.out.println("Invalid Operator");
+
+		}
+
+
 	}
 
 }
