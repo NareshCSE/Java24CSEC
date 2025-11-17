34b509920277ea9d9855c8f27de2c45f53ec05fe
kaumudicodes <24wh1a05e6@bvrithyderabad.edu.in>
2025-11-17 11:55:34 +0530

    Add files via upload

---
+++ 24WH1A05E6/Pattern2.java
@@ -0,0 +1,25 @@
+package Kau;
+import java.util.*;
+public class Pattern2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+        System.out.print("Enter number of rows: ");
+        int n = sc.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+            // print spaces
+            for (int s = 1; s <= n - i; s++) {
+                System.out.print(" ");
+            }
+            // print stars
+            for (int j = 1; j <= i; j++) {
+                System.out.print("#");
+            }
+            System.out.println();
+        }
+
+	}
+
+}
