10d517b2cfae1dbf57a821e62d13764bdaa7a518
gangushirisha02 <gangushirisha2007@gmail.com>
2025-11-18 21:33:49 +0530

    Add files via upload

---
+++ 25wh5a0520/Ecommerce_HashSet.java
@@ -0,0 +1,86 @@
+package java_25wh5a0520;
+import java.util.HashSet;
+import java.util.Set;
+
+class Product {
+	private int id;
+	private String name;
+	private double price;
+	public Product(int id,String name,double price) {
+		this.id=id;
+		this.name=name;
+		this.price=price;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public double getPrice() {
+		return price;
+	}
+	public void setPrice(double price) {
+		this.price = price;
+	}
+	
+public  boolean equals(Object obj)
+{
+if(this== obj)return true;
+if(obj==null||getClass()!=obj.getClass())return false;
+Product product=(Product) obj;
+return id== product.id;
+}
+public int hashCode() {
+	return Integer.hashCode(id);
+}
+public String toString() {
+	return "Product{id="+id+", name='"+name+"',price="+price+"}";
+}
+}
+interface Ecommerce {
+    void addProduct(Product product);
+    Set<Product> getAllProducts();
+    //Order placeOrder(Set<product>);
+    double generateBill(int orderId);
+}
+class Seller1 implements Ecommerce{
+	private Set<Product> productSet;
+	public Seller1() {
+		this.productSet=new HashSet<>();
+	}
+	public void addProduct(Product product) {
+		if(productSet.add(product))
+			System.out.println("product added"+product.getName());
+		else
+			System.out.println("poduct with id: "+product.getId()+" is already in the set");
+	}
+	public Set<Product> getAllProducts(){
+		return this.productSet;
+	}
+	public double generateBill(int orderId) {
+		return 0;
+	}
+}
+public class Ecommerce_HashSet {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Seller1 ee=new Seller1();
+		ee.addProduct(new Product(1,"mobile",20000.00));
+		ee.addProduct(new Product(2,"car",15000.00));
+		ee.addProduct(new Product(3,"ipad",45000.00));
+		ee.addProduct(new Product(4,"watch",5678.00));
+		//try adding duplicates
+		ee.addProduct(new Product(1,"mobile new",45670.00));
+		System.out.println("all products: ");
+		ee.getAllProducts().forEach(System.out::println);
+	}
+
+}
