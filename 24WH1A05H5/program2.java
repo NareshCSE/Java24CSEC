adbc828b2cd9e810f8a516b4e189b6be85977a64
Shivaniavunoori <24wh1a05h5@bvrithyderabad.edu.in>
2025-11-19 18:33:51 +0530

    Add files via upload

---
+++ 24WH1A05H5/program2.java
@@ -0,0 +1,34 @@
+package assignment;
+import java.util.Scanner;
+public class program2 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("enter first number:");
+		double a = sc.nextDouble();
+		System.out.println("enter second number:");
+		double b = sc.nextDouble();
+		System.out.println("enter operator(+,-,*,/):");
+		char op = sc.next().charAt(0);
+		switch (op) {
+		case '+':
+			System.out.println("result:" + (a + b));
+			break;
+		case '-':
+			System.out.println("result:" + (a - b));
+			break;
+		case '*':
+			System.out.println("result:" + (a * b));
+			break;
+		case '/':
+			System.out.println("result:" + (a / b));
+			break;
+		default:
+			System.out.println("invalid");
+		}
+		sc.close();
+	}
+}
+	
+	
+
+
