9fe779884f4d5328082b72ce2e0cbe22b7934ae7
snigdhagatla <25wh5a0516@bvrithyderabad.edu.in>
2025-11-20 21:04:28 +0530

    Add files via upload

---
+++ 25wh5a0516/electricitybill.java
@@ -0,0 +1,47 @@
+package javademo1;
+import java.util.*;
+public class electricitybill {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		   
+		        Scanner sc = new Scanner(System.in);
+
+		        // Input old and current readings
+		        System.out.print("Enter old reading: ");
+		        int oldReading = sc.nextInt();
+
+		        System.out.print("Enter current reading: ");
+		        int currentReading = sc.nextInt();
+
+		        // Units consumed
+		        int units = currentReading - oldReading;
+		        int bill = 0;
+
+		        if (units <= 50) {
+		            bill = units * 1;
+		        } 
+		        else if (units <= 100) {
+		            bill = (50 * 1) + (units - 50) * 2;
+		        } 
+		        else if (units <= 200) {
+		            bill = (50 * 1) + (50 * 2) + (units - 100) * 3;
+		        } 
+		        else if (units <= 400) {
+		            bill = (50 * 1) + (50 * 2) + (100 * 3) + (units - 200) * 4;
+		        } 
+		        else { 
+		            bill = (50 * 1) + (50 * 2) + (100 * 3) + (200 * 4) + (units - 400) * 5;
+		        }
+
+		        
+		        System.out.println("Total units consumed: " + units);
+		        System.out.println("Electricity Bill: Rs." + bill);
+
+		        sc.close();
+		    }
+		
+	
+}
