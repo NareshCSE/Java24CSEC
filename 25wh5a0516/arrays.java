dede41664e69b90f4216615433d4081a56049fda
snigdhagatla <25wh5a0516@bvrithyderabad.edu.in>
2025-11-20 21:47:18 +0530

    Add files via upload

---
+++ 25wh5a0516/arrays.java
@@ -0,0 +1,20 @@
+package assignment;
+import java.util.scanner;
+public class arrays {
+
+
+	    public static void main(String[] args) {
+	        int[] numbers = {1, 5, 2, 3, 5, 4, 2, 6, 1};
+
+	        System.out.println("Duplicate elements in the array:");
+
+	        for (int i = 0; i < numbers.length; i++) {
+	            for (int j = i + 1; j < numbers.length; j++) {
+	                if (numbers[i] == numbers[j]) {
+	                    System.out.println(numbers[i]);
+	                    break; 
+	                }
+	            }
+	        }
+	    }
+	}
