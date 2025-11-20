4ac27fa81b676939f89b996645f92ebbad0928dc
snigdhagatla <25wh5a0516@bvrithyderabad.edu.in>
2025-11-20 22:01:50 +0530

    Add files via upload

---
+++ 25wh5a0516/maxevennum.java
@@ -0,0 +1,43 @@
+package assignment;
+import java.util.*;
+public class maxevennum {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	
+		        Scanner sc = new Scanner(System.in);
+		        System.out.print("Enter a string: ");
+		        String str = sc.nextLine();
+		        sc.close();
+
+		        int[] digits = new int[10];
+		        for (int i = 0; i < str.length(); i++) {
+		            char ch = str.charAt(i);
+		            if (Character.isDigit(ch)) {
+		                digits[ch - '0'] = 1;
+		            }
+		        }
+
+		        int evenDigit = -1;
+		        for (int i = 0; i <= 9; i++) {
+		            if (digits[i] == 1 && i % 2 == 0) {
+		                evenDigit = i;
+		            }
+		        }
+
+		        if (evenDigit == -1) {
+		            System.out.println(-1);
+		            return;
+		        }
+
+		        System.out.print("Largest even number: ");
+		        for (int i = 9; i >= 0; i--) {
+		            if (digits[i] == 1 && i != evenDigit) {
+		                System.out.print(i);
+		            }
+		        }
+		        System.out.println(evenDigit);
+		    }
+		
+
+}
