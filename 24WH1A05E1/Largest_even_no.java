54c7db43e606cc749c8e1b9b3f1ae72c46774a86
M. LAKSHMI TANOOJA <tanoojaml@gmail.com>
2025-11-25 04:50:57 +0530

    Update Largest_even_no.java

--- 24WH1A05E1/Largest_even_no.java
+++ 24WH1A05E1/Largest_even_no.java
@@ -1,13 +1,23 @@
+import java.util.Scanner;
+
 public class Largest_even_no {
+
     public static void main(String[] args) {
-        String str = "ad%#678#@";   
-        boolean[] present = new boolean[10]; 
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter a string:");
+        String str = sc.nextLine();
+
+        boolean[] present = new boolean[10];
+
+        // Extract unique digits
         for (int i = 0; i < str.length(); i++) {
             char ch = str.charAt(i);
             if (Character.isDigit(ch)) {
                 present[ch - '0'] = true;
             }
         }
+
+        // Find smallest even digit to place at the end
         int smallestEven = -1;
         for (int d = 0; d <= 9; d++) {
             if (present[d] && d % 2 == 0) {
@@ -16,21 +26,23 @@ public class Largest_even_no {
             }
         }
 
-        
         if (smallestEven == -1) {
             System.out.println(-1);
             return;
         }
 
-        System.out.println("largest even no:");
-        
+        StringBuilder sb = new StringBuilder();
+
+        // Append digits from largest to smallest (except the final even digit)
         for (int d = 9; d >= 0; d--) {
             if (present[d] && d != smallestEven) {
-                System.out.print(d);
+                sb.append(d);
             }
         }
 
-     System.out.println(smallestEven);
-       
+        // Last digit is the smallest even
+        sb.append(smallestEven);
+
+        System.out.println(sb.toString());
     }
 }
